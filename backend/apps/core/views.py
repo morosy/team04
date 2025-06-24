dc6b826a416cc2030a37cbefbad131d28ca6ea9a
Shunsuke MOROZUMI <137362288+morosy@users.noreply.github.com>
2025-06-24 12:37:26 +0900

    カレントディレクトリの変更

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+'''
 def home(request):
     return render(request, 'core/home.html')
+'''
+
+'''
+    data: 2025/06/23
+    カレントディレクトリをfrontendに変更
+'''
+def home(request):
+    return render(request, 'home.html')
