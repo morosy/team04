795489e5cbb4f5979a5208f78ad1870900ab70c6
kurasora <sorakuraishi@gmail.com>
2025-06-24 13:14:30 +0900

    ランキング処理とフレンド処理のルーティング

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('ranking/', views.ranking_view, name='ranking'),
+    path('friend/request/', views.friend_request_view, name='friend_request'),
+    path('friend/accept/', views.friend_accept_view, name='friend_accept'),
+    path('friend/decline/', views.friend_decline_view, name='friend_decline'),
 ]
