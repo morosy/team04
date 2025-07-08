0fbed3199fe8ad87ab8c4bae58bab360b8efdcb6
Shunsuke MOROZUMI <137362288+morosy@users.noreply.github.com>
2025-07-08 14:17:49 +0900

    保険

---
+++ docker/web/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+/wait-for-it.sh db:3306 --timeout=30 --strict -- echo "Database is up"
+
+# Django の起動
+python /app/backend/manage.py migrate 
+python /app/backend/manage.py runserver 0.0.0.0:8000 
+
+
+# Node アプリの起動（必要なら）
+# cd frontend
+# npm start
+
+# wait によってバックグラウンドプロセスを維持
+wait
