d49857f1ff4bd0292a044fcab80326d89b265bbf
Shunsuke MOROZUMI <137362288+morosy@users.noreply.github.com>
2025-07-08 10:26:21 +0900

    テストデータ挿入用スクリプトの更新

--- docker/init/init.py
+++ docker/init/init.py
@@ -1,20 +1,29 @@
 # initialization.py
 import init_users
 import init_user_result
+import init_horse_status
 import time
 
 def initialize_all():
     print("=== 待機中 ===")
     time.sleep(5)
+
+    print("=== 馬データ挿入中 ===")
+    init_horse_status.initialize_horse_status()
+    print("=== 馬データ挿入完了 ===")
+
     print("=== ユーザー挿入中 ===")
     init_users.insert_users()
+    print("=== ユーザー挿入完了 ===")
 
-    print("=== 挿入完了 ===")
     print("=== 待機中 ===")
     time.sleep(5)
 
     print("=== ユーザー結果挿入中 ===")
     init_user_result.insert_from_csv()
+    print("=== ユーザー結果挿入完了 ===")
+
+
 
 if __name__ == "__main__":
     initialize_all()
